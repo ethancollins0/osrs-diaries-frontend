a97fe232db63d7db77a40cc563d93cf34ba4623c
Ethan Collins <ethantcollins98@gmail.com>
2019-12-03 18:03:11 -0700

    fixes stretched background image

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -18,17 +18,17 @@ export default {
         flex-direction: row;
         justify-content: space-around;
         background: #000;
-        
+
           .background {
             display: flex;
             justify-content: center;
-            width: 60vw;
-            max-width: 700px;
-            min-width: 400px;
-            height: 100%;
+            height: 320px;
             min-height: 100vh;
             background: url('https://runescape.com/img/rsp777/bg2.jpg');
-            background-size: 100% 50%;
+            background-size: 100% 320px;
+            width: 40vw;
+            max-width: 700px;
+            min-width: 400px;
             background-repeat: repeat-y;
             margin: 0;
             padding: 0;
